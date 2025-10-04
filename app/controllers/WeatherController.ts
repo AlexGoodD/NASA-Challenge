ef8c999c912652a734d0f3043ae544fe56aa476d
theramagnoli <sergioramagnoli@gmail.com>
2025-10-04 16:16:36 -0600

    Se corrigen errores en WeatherController

--- app/controllers/WeatherController.ts
+++ app/controllers/WeatherController.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { getWeatherValidator } from '#validators/weather'
-import { fetchWeatherApi } from 'openmeteo';
+import { fetchWeatherApi } from 'openmeteo'
 
 export default class WeatherController {
   public async index({ inertia, request }: HttpContext) {
@@ -10,30 +10,46 @@ export default class WeatherController {
     return inertia.render('Dashboard')
   }
 
-  public async search({request} : HttpContext){
-    const {latitude, longitude} = request.body()
+  public async search({ request }: HttpContext) {
+    const { latitude, longitude } = request.body()
     console.log(latitude, longitude)
     const params = {
-      "latitude": Number(latitude),
-      "longitude": Number(longitude),
-      "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "precipitation_probability_max", "wind_speed_10m_max"],
-      "hourly": ["temperature_2m", "wind_speed_10m", "relative_humidity_2m", "precipitation_probability", "apparent_temperature", "dew_point_2m"],
-      "timezone": "America/Chicago",
-      "forecast_days": 1,
-    };
+      latitude: Number(latitude),
+      longitude: Number(longitude),
+      daily: [
+        'weather_code',
+        'temperature_2m_max',
+        'temperature_2m_min',
+        'precipitation_probability_max',
+        'wind_speed_10m_max',
+      ],
+      hourly: [
+        'temperature_2m',
+        'wind_speed_10m',
+        'relative_humidity_2m',
+        'precipitation_probability',
+        'apparent_temperature',
+        'dew_point_2m',
+      ],
+      timezone: 'America/Chicago',
+      forecast_days: 1,
+    }
 
-    const url = "https://api.open-meteo.com/v1/forecast";
-    const responses = await fetchWeatherApi(url, params);
+    const url = 'https://api.open-meteo.com/v1/forecast'
+    const responses = await fetchWeatherApi(url, params)
     const response = responses[0]
 
     const hourly = response.hourly()!
     const daily = response.daily()!
-    const utcOffsetSeconds = response.utcOffsetSeconds();
+    const utcOffsetSeconds = response.utcOffsetSeconds()
 
     const weatherData = {
       hourly: {
-        time: [...Array((Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval())].map(
-          (_, i) => new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
+        time: [
+          ...Array((Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval()),
+        ].map(
+          (_, i) =>
+            new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
         ),
         temperature_2m: hourly.variables(0)!.valuesArray(),
         wind_speed_10m: hourly.variables(1)!.valuesArray(),
@@ -44,7 +60,8 @@ export default class WeatherController {
       },
       daily: {
         time: [...Array((Number(daily.timeEnd()) - Number(daily.time())) / daily.interval())].map(
-          (_, i) => new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000)
+          (_, i) =>
+            new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000)
         ),
         weather_code: daily.variables(0)!.valuesArray(),
         temperature_2m_max: daily.variables(1)!.valuesArray(),
@@ -52,13 +69,8 @@ export default class WeatherController {
         precipitation_probability_max: daily.variables(3)!.valuesArray(),
         wind_speed_10m_max: daily.variables(4)!.valuesArray(),
       },
-    };
-
-
+    }
 
     return weatherData
-
-
-
   }
 }
